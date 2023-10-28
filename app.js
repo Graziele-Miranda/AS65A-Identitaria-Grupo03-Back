96954b5a7fc3297d34bcad5ab2e335df749cdf6b
RafaelAJustino <rafael.angelo.justino@gmail.com>
2023-10-28 14:14:39 -0300

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 require('dotenv').config();
 
 var app = express();
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
